aa6cf8ef3a016942d8dccb0f2c7a4d18aaa1e54b
Carolina Lopes <carolina.lopes@kcl.ac.uk>
2020-06-15 21:58:51 +0200

    Move area calculation to Polygon and correct mistakes

--- GeometryProcessing/polygon.hpp
+++ GeometryProcessing/polygon.hpp
@@ -20,6 +20,44 @@ public:
     Polygon(){};
 };
 
+double area(Polygon p) {
+    auto vertices = p.vertices;
+    auto N = vertices.size();
+    double sumArea = 0.0;
+        for (int i=0;i<N;i++){
+                double xi = vertices[i][0];
+                double yi = vertices[i][1];
+                double xii = vertices[(i+1)%N][0];
+                double yii = vertices[(i+1)%N][1];
+
+                sumArea = sumArea + ((xi*yii) - (xii*yi));
+        }
+    auto A = 0.5 * sumArea;
+    return A;
+}
+
+double distanceIntegral(Polygon p,Vector Pi) {
+    auto vertices = p.vertices;
+    if(!vertices.size()) return 0;
+    double I = 0;
+    Vector c[3];
+    c[0] = vertices[0];
+    for(int i = 1; i < int(vertices.size()) - 1; i ++) {
+        if(vertices[i][2] != 0) std::cout << "non zero z" << std::endl;
+        double x = 0;
+        c[1] = vertices[i];
+        c[2] = vertices[i+1];
+        for(int k = 0; k < 3; k ++) {
+            for(int l = k; l < 3; l ++) {
+                x += dot(c[k] - Pi, c[l] - Pi);
+            }
+        }
+        double T = cross(c[0] - c[1], c[2] - c[1])[2]/2;
+        I += T * x;
+    }
+    return I / 6;
+}
+
 class VoronoiDiagram {
 public: 
     Polygon points;
